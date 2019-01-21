19ae0a58facd843f39cf91bcf8b4498a61e75771
DefinitelyNotAShark <definitelynotasharkohno@gmail.com>
2019-01-21 15:54:24 -0600

    Putting stuff in it

---
+++ ElementGun/Assets/DebugFlash.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugFlash : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem[] shootParticles;
+
+    [SerializeField]
+    private float coolDownTime;
+
+    private bool coroutineStarted = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        shootParticles = GetComponentsInChildren<ParticleSystem>();//get all our particle system
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetAxis("Jump") > 0)
+        {
+            if (!coroutineStarted)
+                StartCoroutine(Shoot());
+        }
+	}
+
+    private IEnumerator Shoot()
+    {
+        coroutineStarted = true;
+        foreach(ParticleSystem p in shootParticles)
+        {
+            p.Play();
+        }
+
+        yield return new WaitForSeconds(coolDownTime);
+        coroutineStarted = false;
+    }
+}
